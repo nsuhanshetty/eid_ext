f8f011847f52aaec6196328d25d8ea550f2229ea
nsuhanshetty <nsuhanshetty>
2013-06-13 13:19:12 +0530

    13-06-2013
    Changes to pdf viewer

--- eid/WinformPdfViewer.Designer.cs
+++ eid/WinformPdfViewer.Designer.cs
@@ -124,6 +124,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(727, 488);
+            this.ControlBox = false;
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.webBrowser1);
